f2405ff533512cf3582052d620bcc6d291f9220c
tsarakin <timon.sarakinis@volvocars.com>
2024-05-06 08:45:13 +0200

    set relativeNumbers to true

--- init.lua
+++ init.lua
@@ -107,7 +107,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -854,7 +854,6 @@ require('lazy').setup({
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
 
   { import = 'custom.plugins' },
-
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- vimrc.vim
+++ vimrc.vim
@@ -2,4 +2,7 @@
 "neo-tree"
 command Nt Neotree
 
-
+"enable fzf in vim"
+set rtp+=~/.fzf
+"case insensitivy when searching / ? etc" 
+set smartcase
