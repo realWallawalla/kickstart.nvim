98768e59e2b05a47b7f8a84f8d870cc0512558bf
realWallawalla <elcheapoinvest@gmail.com>
2026-03-19 18:21:02 +0100

    update tresitter plugin. add remote server yanking

--- init.lua
+++ init.lua
@@ -120,6 +120,21 @@ vim.opt.showmode = false
 --  See `:help 'clipboard'`
 vim.opt.clipboard = 'unnamedplus'
 
+-- Use OSC 52 to bridge the SSH gap (Requires Neovim 0.10+)
+if vim.env.SSH_TTY then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+      ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+      ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+    },
+  }
+end
+
 -- Enable break indent
 vim.opt.breakindent = true
 
--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-treesitter_langs = {
+local treesitter_langs = {
   'bash',
   'c',
   'diff',
@@ -18,87 +18,31 @@ treesitter_langs = {
 }
 
 return {
-  { -- Highlight, edit, and navigate code
+  {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
     opts = {
       ensure_installed = treesitter_langs,
-      -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
         enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
-      -- below is for plugin molten
+      textobjects = {
+        -- ... keep your existing textobjects config here ...
+      },
     },
     config = function(_, opts)
-      -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+      -- Fix: The old 'nvim-treesitter.configs' is gone.
+      -- We now use the main module's setup.
+      require('nvim-treesitter').setup(opts)
 
-      -- Prefer git instead of curl in order to improve connectivity in some environments
+      -- Optional: prefer git for installs
       require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-    end,
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    dependencies = { 'nvim-treesitter' },
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        sync_install = false,
-        ensure_installed = treesitter_langs,
-        -- Autoinstall languages that are not installed
-        auto_install = true,
-        ignore_install = {},
-
-        -- ... other ts config
-        textobjects = {
-          move = {
-            enable = true,
-            set_jumps = false, -- you can change this if you want.
-            goto_next_start = {
-              --- ... other keymaps
-              [']b'] = { query = '@code_cell.inner', desc = 'next code block' },
-            },
-            goto_previous_start = {
-              --- ... other keymaps
-              ['[b'] = { query = '@code_cell.inner', desc = 'previous code block' },
-            },
-          },
-          select = {
-            enable = true,
-            lookahead = true, -- you can change this if you want
-            keymaps = {
-              --- ... other keymaps
-              ['ib'] = { query = '@code_cell.inner', desc = 'in block' },
-              ['ab'] = { query = '@code_cell.outer', desc = 'around block' },
-            },
-          },
-          swap = { -- Swap only works with code blocks that are under the same
-            -- markdown header
-            enable = true,
-            swap_next = {
-              --- ... other keymap
-              ['<leader>sbl'] = '@code_cell.outer',
-            },
-            swap_previous = {
-              --- ... other keymap
-              ['<leader>sbh'] = '@code_cell.outer',
-            },
-          },
-        },
-      }
     end,
   },
 }
